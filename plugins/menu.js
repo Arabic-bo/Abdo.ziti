dd9874d80b45828d562469a2812f177a9aa6492a
Abdoziti <156618732+Abdoziti@users.noreply.github.com>
2024-02-18 13:09:01 +0100

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -138,7 +138,7 @@ contextInfo: {
 externalAdReply: {
 title: 'ALEXA BOT ♥',
 body: "مجموعتي على الواتساب 💖",
-thumbnailUrl: 'https://chat.whatsapp.com/Fk1mt0Ui4AV0KEA8Dt49Xz',
+thumbnailUrl: 'https://telegra.ph/file/4fbd9c7a53a10fd89d4e5.jpg',
 sourceUrl: 'https://chat.whatsapp.com/Fk1mt0Ui4AV0KEA8Dt49Xz',
 mediaType: 1,
 renderLargerThumbnail: true
